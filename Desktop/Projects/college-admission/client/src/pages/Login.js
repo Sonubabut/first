a61c17caa2faadb5d7a03fb17fa3a6b1395f5edb
sonu <tsonubabu0@gmail.com>
2023-08-14 13:17:01 +0530

    first commit

---
+++ Desktop/Projects/college-admission/client/src/pages/Login.js
@@ -0,0 +1,91 @@
+import React, { useState } from "react";
+import './login.css';
+import {
+  MDBCard,
+  MDBCardBody,
+  MDBInput,
+  MDBCardFooter,
+  MDBValidation,
+  MDBBtn,
+  MDBIcon,
+  MDBCarouselItem
+} from "mdb-react-ui-kit";
+import { Link } from "react-router-dom";
+
+
+const initialState = {
+  email: "",
+  password: "",
+};
+
+const Login = () => {
+  const [formValue, setFormValue] = useState(initialState);
+  const { email, password } = formValue;
+  const handleSubmit = () => {};
+  const onInputChange = () => {};
+  return (
+    
+    <div
+      style={{
+        margin: "auto",
+        padding: "15px",
+        maxWidth: "450px",
+        alignContent: "center",
+        marginTop: "120px",
+      }}
+    >
+        <MDBCarouselItem
+        className='w-100 d-block'
+        src='https://images.pexels.com/photos/158756/flowers-plants-korea-nature-158756.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+        alt='...'
+      />
+      <MDBCard alignment="center">
+        <MDBIcon fas icon="user-circle" className="fa-2x" />
+        <h5>Sign In</h5>
+        <MDBCardBody>
+          <MDBValidation onSubmit={handleSubmit} noValidate className="row g-3">
+            <div className="col-md-12">
+              <MDBInput
+                label="Email"
+                type="email"
+                value={email}
+                name="email"
+                onChange={onInputChange}
+                required
+                invalid
+                validation="Please provide your email"
+              />
+            </div>
+            <div className="col-md-12">
+              <MDBInput
+                label="Password"
+                type="password"
+                value={password}
+                name="password"
+                onChange={onInputChange}
+                required
+                invalid
+                validation="Please provide your password"
+              />
+            </div>
+            <div className="col-12">
+              <MDBBtn style={{ width: "100%" }} className="mt-2">
+                
+                Login
+              </MDBBtn>
+            </div>
+          </MDBValidation>
+          <br />
+
+        </MDBCardBody>
+        <MDBCardFooter>
+          <Link to="/register">
+            <p>Don't have an account ? Sign Up</p>
+          </Link>
+        </MDBCardFooter>
+      </MDBCard>
+    </div>
+  );
+};
+
+export default Login;
